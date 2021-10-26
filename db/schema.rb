8b8853cc00635916b64c1cb10ea1ead45dadf3ba
fatiq-butt <fatiq.butt@square63.org>
2021-10-26 13:02:39 +0500

    Token Schema added & added mail configuration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_043529) do
-
+ActiveRecord::Schema.define(version: 2021_10_26_073833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +24,10 @@ ActiveRecord::Schema.define(version: 2021_10_25_043529) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
