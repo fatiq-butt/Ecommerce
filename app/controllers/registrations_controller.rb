2e7fc558a926b45f08f133821419d0f9f4d3586a
fatiq-butt <fatiq.butt@square63.org>
2021-10-26 11:50:18 +0500

    Signup Form with validations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :check_resource, only: [:create]
   def new
     build_resource
     respond_to do |format|
       format.js
     end    
+  end  
+  def check_resource
+    build_resource(sign_up_params)  
+    unless resource.validate
+      flash[:errors] = resource.errors.full_messages
+      render "devise/sessions/new"
+    end       
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2021_10_25_043529) do
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -20,8 +23,8 @@ ActiveRecord::Schema.define(version: 2021_10_25_043529) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "first_name"
-    t.string "last_name"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
