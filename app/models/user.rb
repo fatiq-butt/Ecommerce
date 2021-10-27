92b250d54e3703ed3ada4fe28f0431ccb9810b0f
fatiq-butt <fatiq.butt@square63.org>
2021-10-27 10:43:10 +0500

    Removing extra spaces from code

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,5 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, format: {with: /(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}/, message: "The password should be atleast 8 characters long. To make it stronger, use upper and lower case letters, numbers, and symbols like !*?$%^&)."}
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
 end
