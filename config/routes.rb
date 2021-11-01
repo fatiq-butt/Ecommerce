ff4f6a46fe2c192d756a502d95c6c0b3d57bd2e2
fatiq-butt <fatiq.butt@square63.org>
2021-11-01 12:41:41 +0500

    Created namespace for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
-  scope '/admin' do
+  # scope '/admin' do
+  #   resources :users
+  # end
+  namespace :admin do
     resources :users
   end
   get '/users', to: redirect('users/sign_in')
