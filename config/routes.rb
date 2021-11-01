5b2d72b8dfa1ca9aef0b3c3c82311efa07f8ba30
fatiq-butt <fatiq.butt@square63.org>
2021-11-01 12:48:00 +0500

    Remove Extra Spaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
-  # scope '/admin' do
-  #   resources :users
-  # end
+
   namespace :admin do
     resources :users
   end
+  
   get '/users', to: redirect('users/sign_in')
   root to: 'main#index'
 end
