358544892855c715962d6a2818684fe7b1e67275
fatiq-butt <fatiq.butt@square63.org>
2021-11-01 17:09:44 +0500

    Removed DS_STore

--- app/javascript/custom/sign_up_click.js
+++ app/javascript/custom/sign_up_click.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).on('turbolinks:load', function () { 
   if ($("#errors").length > 0)
   {
     $("#errors")[0].click()
   }
-});
+ });
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
 
+  regex = /(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}/
+  
   validates :first_name, :last_name, presence: true
-  validates :password, format: {with: /(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}/,
-   message: "The password should be atleast 8 characters long. To make it stronger, use upper and lower case letters, numbers, and symbols like !*?$%^&)."}
+  validates :password, format: {with: regex,
+   message: " should be atleast 8 characters long. To make it stronger, use upper and lower case letters, numbers, and symbols like !*?$%^&)."}
          
 end
