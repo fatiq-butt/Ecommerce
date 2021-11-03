b59539e2df9907745aa2a578604ea9f0d8096fcf
fatiq-butt <fatiq.butt@square63.org>
2021-11-03 15:42:45 +0500

    Created Separate Folder for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
-  scope '/admin' do
+  
+  namespace :admin do
     resources :users
   end
+  
   get '/users', to: redirect('users/sign_in')
   root to: 'main#index'
 end
