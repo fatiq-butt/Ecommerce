99d99725ce4ec4116add6cce58d0b5e48828f1dc
fatiq-butt <fatiq.butt@square63.org>
2021-11-05 11:38:51 +0500

    Corrected synatx for devise params senitizer, intendation in Session_new, key value syntax in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users, controllers: {registrations: "registrations"}
   root to: 'main#index'
   get '/users', to: redirect('users/sign_in')
 end
