38714db54a11a4bd505db99bc6c497274ef98266
fatiq-butt <fatiq.butt@square63.org>
2021-11-05 17:40:23 +0500

    Added product, fix product delete, added product show

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < AdminController
+  before_action :set_product, only: %i[ show edit update destroy ]
   def index
     @products = Product.all
   end
@@ -10,15 +11,39 @@ class Admin::ProductsController < AdminController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to admin_products_path
+      redirect_to admin_products_path, notice: "New Product Added Successfully.."
     else
       render 'new'
     end  
   end
 
+  def show;end
+
+  def edit;end
+
+  def destroy
+    respond_to do |format|
+      @product.destroy
+      format.js
+    end    
+  end
+
+  def update
+    if @product.update(product_params)
+      flash[:notice] = "Product Updated Successfully.."
+      redirect_to admin_products_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
   def product_params
-    params.require(:product).permit(:title, :price, :description, :status,images: [])
+    params.require(:product).permit(:title, :price, :description, :status, images: [])
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
-  has_many_attached :images  
+  has_many_attached :images 
 
   STATUS = ["Publish", "Draft", "Pending"]
   
   validates :price, numericality: {only_integer: false}, presence: true 
-  validates :title, :description, :status, presence: true 
+  validates :title, :description, :status, :images, presence: true 
 
 end
