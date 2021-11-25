d9537a13e97c7563d40e60bce6683511c226bd74
fatiq-butt <fatiq.butt@square63.org>
2021-11-25 20:15:13 +0500

    Fix select 2 issue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,12 @@
-import 'bootstrap'
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
+import 'bootstrap';
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
 require("channels")
 require("jquery");
 global.$ = jQuery;
 require("custom/sign_up_click");
+
 import "select2";
 import 'select2/dist/css/select2.css';
 
