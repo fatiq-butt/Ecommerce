c96b7b3ad1deb680c735fe18f60c3d4a65bd83e4
fatiq-butt <fatiq.butt@square63.org>
2021-11-25 17:21:55 +0500

    PR Fixes

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,9 @@ class Product < ApplicationRecord
 
   has_many_attached :images, dependent: :destroy
 
-  pg_search_scope :global_product_search, against: [:id, :title, :price, :description, :status], using: {tsearch: {prefix: true}}
+  pg_search_scope :global_product_search, against: [:id, :title, :price, :description, :status], using: { tsearch: { prefix: true } }
 
-  STATUS = ["Publish", "Draft", "Pending"].freeze
+  STATUSES = ["Publish", "Draft", "Pending"].freeze
 
   validates :price, numericality: true, presence: true
   validates :title, :description, :status, :images, presence: true 
