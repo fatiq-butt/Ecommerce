6301cbd34ab6c5370b7a5604bbc19969ac82269b
fatiq-butt <fatiq.butt@square63.org>
2021-11-26 12:54:08 +0500

    Fixed naming in controller concern

--- app/controllers/concerns/filter_records.rb
+++ app/controllers/concerns/filter_records.rb
@@ -2,9 +2,9 @@ module FilterRecords
   extend ActiveSupport::Concern
 
   def sort_page_filter(model)
-    @records = model.all
-    @records = @records.global_search(params[:search]) if params[:search].present?
-    @records = @records.order(params[:sort] => params[:order]) if params[:order].present?
-    @pagy, @records = pagy(@records, items: 5)
+    records = model.all
+    records = records.global_search(params[:search]) if params[:search].present?
+    records = records.order(params[:sort] => params[:order]) if params[:order].present?
+    pagy(records, items: 5)
   end
 end
