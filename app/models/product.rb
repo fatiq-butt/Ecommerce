c2b4bb8f7ebfbeb7797422d4449bafebf9613020
fatiq-butt <fatiq.butt@square63.org>
2021-12-01 11:11:22 +0500

    Fixed category deletion null constraint

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,7 @@ class Product < ApplicationRecord
   has_many :orders, through: :receipts
   has_rich_text :description
 
-  belongs_to :category
+  belongs_to :category, optional: true
 
   pg_search_scope :global_search, against: [:id, :title, :price, :description, :status], using: { tsearch: { prefix: true } }
 
---
+++ db/migrate/20211201060645_change_category_from_products.rb
@@ -0,0 +1,5 @@
+class ChangeCategoryFromProducts < ActiveRecord::Migration[6.1]
+  def change
+    change_column_null :products, :category_id, true
+  end
+end
